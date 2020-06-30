436c5ceb744a4a7ae7a0b2d294eaea95afe0d2ec
Lorraine Fadini <techpark.lorraine.fadini@gmail.com>
2020-06-30 02:26:53 -0300

    WIP- geral

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  lineChart();
+
+  $(window).resize(function() {
+    window.lineChart.redraw();
+  });
+});
+console.log('entrou aqui 1');
+
+pais = '';
+mapa = {};
+list = JSON.parse(localStorage.getItem('lista'));
+
+function lineChart() {
+  console.log('list', list);
+  console.log('entrou aqui 3');
+  window.lineChart = Morris.Line({
+    element: 'line-chart',
+    data: list.Afghanistan,
+    xkey: 'date',
+    ykeys: ['confirmed', 'deaths', 'recovered'],
+    labels: ['Infectados', 'Mortos', 'recuperados'],
+    lineColors: ['#ffff00','#ff3321', '#1e88e5'],
+    lineWidth: '3px',
+    resize: true,
+    redraw: true
+  });
+}
+
+function selecao() {
+
+  let pSelect = document.getElementById('cselect');
+  //console.log('pSelect', pSelect);
+ // console.log(cselect.selectedIndex);
+  this.pais = cselect.options[cselect.selectedIndex].value;
+  var nome = cselect.options[cselect.selectedIndex].value;
+  mapa = {};
+  mapa[nome] = [];
+  console.log('teste mapa', mapa);
+  console.log(this.pais);
+}
